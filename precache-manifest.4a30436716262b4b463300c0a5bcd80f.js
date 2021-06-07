879b777cdf9f70dce636b227a2d6142db9ceeb20
Esteban Velasquez <ev@oiga.com>
2021-06-06 21:38:15 -0500

    Updates

---
+++ precache-manifest.4a30436716262b4b463300c0a5bcd80f.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5907c4ffe4bdd0c8c2988877678fbe75",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bef692e2af0766264bdc",
+    "url": "./static/css/3.eeee29c1.chunk.css"
+  },
+  {
+    "revision": "63dfa3468bef8dccb1a1",
+    "url": "./static/css/main.c072ba4e.chunk.css"
+  },
+  {
+    "revision": "30fd47d9b6636cfd9f57",
+    "url": "./static/js/0.8735f8e3.chunk.js"
+  },
+  {
+    "revision": "fb0babf598e05108f7a0",
+    "url": "./static/js/10.02b9db0c.chunk.js"
+  },
+  {
+    "revision": "bef692e2af0766264bdc",
+    "url": "./static/js/3.c26100a1.chunk.js"
+  },
+  {
+    "revision": "eb575425c07b55b7e8cdfc9f5b35fc08",
+    "url": "./static/js/3.c26100a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d15ddba24888f605a9f",
+    "url": "./static/js/4.b6b7911d.chunk.js"
+  },
+  {
+    "revision": "0880ea1221f6f59c1702",
+    "url": "./static/js/5.b758f107.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "./static/js/5.b758f107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a84dc7b6ac682bff3d49",
+    "url": "./static/js/6.04b34304.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "./static/js/6.04b34304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb909fc7319b83a81dd",
+    "url": "./static/js/7.096d9073.chunk.js"
+  },
+  {
+    "revision": "4561328d5e00edaa9e0a",
+    "url": "./static/js/8.9f392562.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "./static/js/8.9f392562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e3a83d54cc38e35cf53",
+    "url": "./static/js/9.3a88bd68.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "./static/js/9.3a88bd68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63dfa3468bef8dccb1a1",
+    "url": "./static/js/main.865e5a54.chunk.js"
+  },
+  {
+    "revision": "da50fb32ff548af45a3b",
+    "url": "./static/js/runtime-main.7d2290f8.js"
+  },
+  {
+    "revision": "73fca47626156d3a68cfea3ae31a8b0d",
+    "url": "./static/media/audio_icon.73fca476.svg"
+  },
+  {
+    "revision": "9b92ad867e59fb170fa839587bec965b",
+    "url": "./static/media/chevron.9b92ad86.svg"
+  },
+  {
+    "revision": "25f47edeec8b1451429199ed4ec5ed15",
+    "url": "./static/media/coralreef_dash.25f47ede.svg"
+  },
+  {
+    "revision": "807818c6bd490ebc00cba8e14f59085b",
+    "url": "./static/media/coralreef_logo.807818c6.svg"
+  },
+  {
+    "revision": "e14aacf3fee4d2391ce75596b46ff40e",
+    "url": "./static/media/fishes.e14aacf3.svg"
+  },
+  {
+    "revision": "6f4c40fbb58a56ebcafc68da6f9186e5",
+    "url": "./static/media/image_icon.6f4c40fb.svg"
+  },
+  {
+    "revision": "a38252c51fb4ace4519b0677d0cbfe41",
+    "url": "./static/media/turtle.a38252c5.svg"
+  },
+  {
+    "revision": "941a917985ce6977da203588bf82c73d",
+    "url": "./static/media/video_icon.941a9179.svg"
+  },
+  {
+    "revision": "0b428370fac63d3b804329548d78b213",
+    "url": "./static/media/wallet_icon.0b428370.svg"
+  }
+]);
