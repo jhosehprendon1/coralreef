d95af31173f32f789c8962e85f82fed0ee6ce0e4
Juan Gutierrez <jg@oiga.com>
2021-06-28 12:01:46 -0400

    Finish landing in progress page

--- js/packages/proko/src/views/static/siteInProgress/index.tsx
+++ js/packages/proko/src/views/static/siteInProgress/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { TwitterCircleFilled } from '@ant-design/icons';
+import { ReactComponent as CoralRefLogo } from '../../../images/coralreef_logo.svg';
 import './index.less';
+
 
 export default (props: any) => {
   return <main>
-    <header className="center">
-      <a href="" rel="noopener">Follow us on Twitter</a>
+    <header className="header center">
+      <CoralRefLogo />
+      <div className="heading">
+        Please Follow us on Twitter for updates comming soon <a href="https://twitter.com/CoralReefNFT?s=09" rel="noopener"><TwitterCircleFilled /></a>
+      </div>
     </header>
   </main>;
 };
